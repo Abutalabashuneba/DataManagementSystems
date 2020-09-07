b1d9f4ba3baf4c97e012681853a2f51367432e22
Kai <chanjunkai99@hotmail.com>
2020-09-07 16:52:33 +0800

    auto identify account type

--- js/login.js
+++ js/login.js
@@ -4,14 +4,14 @@ var userRef = database.ref("account");
 $("#loginForm").submit(function(e){
     e.preventDefault();
     var pass = document.getElementById("pwd").value;
-    var admin = document.getElementById("admin").checked;
+    // var admin = document.getElementById("admin").checked;
     var email = document.getElementById("email").value;
-    var type = undefined;
+    // var type = undefined;
     var validate = false;
     var errorMessage = "";
 
-    if(admin) type = "Admin";
-    else type = "User";
+    // if(admin) type = "Admin";
+    // else type = "User";
 
     userRef.on("value", snap=>{
       var userObj = snap.val();
@@ -21,18 +21,13 @@ $("#loginForm").submit(function(e){
         var k = userKeys[x];
   
         if(email == userObj[k].email || email == userObj[k].username){
-          if(type == userObj[k].type){
-            if(pass == userObj[k].password){
-              validate = true;
-              sessionStorage.setItem("username",userObj[k].username);
-              sessionStorage.setItem("type",userObj[k].type);
-              break;
-            }else{
-              errorMessage = "Wrong password";
-              break;
-            }
+          if(pass == userObj[k].password){
+            validate = true;
+            sessionStorage.setItem("username",userObj[k].username);
+            sessionStorage.setItem("type",userObj[k].type);
+            break;
           }else{
-            errorMessage = "Wrong account type";
+            errorMessage = "Wrong password";
             break;
           }
         }else{
