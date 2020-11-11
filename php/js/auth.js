238352124f6edc3a1ccaefb9b2ca580125b23397
Kai <chanjunkai99@hotmail.com>
2020-11-11 15:07:49 +0800

    removed sensor page

--- php/js/auth.js
+++ php/js/auth.js
@@ -8,7 +8,7 @@ else{
 
     if(sessionStorage.getItem("type") == "Admin"){
         document.getElementById("register").style.display = "block";
-        document.getElementById("sensor").style.display = "block";
+        // document.getElementById("sensor").style.display = "block";
     }
 }
 
